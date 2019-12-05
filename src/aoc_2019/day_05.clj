6f8b36163866f9597cb483aae20812f517d87c0c
nmyers217 <nickbmyers217@gmail.com>
2019-12-05 13:17:40 -0600

    Feat: finish up day 5 part 2

--- src/aoc_2019/day_05.clj
+++ src/aoc_2019/day_05.clj
@@ -15,14 +15,13 @@
   ([data ip]
    {:ip ip ; The instruction pointer
     :memory data ; the program in memory
-    ; How different modes access their data
-    ; NOTE: this is only for reads since writes are always mode 0
+    ;; How different modes access their data
+    ;; NOTE: this is only for reads since writes are always mode 0
     :modes {0 (fn [memory ip] (memory (memory ip)))
             1 (fn [memory ip] (memory ip))}
-    ; All the operations we support
+    ;; All the operations we support
     :ops {1 {:arity :binary :fn + :write? :memory}
           2 {:arity :binary :fn * :write? :memory}
-          ; FIXME: this is hard-coded to always input 1 for now
           3 {:arity nil :fn (comp read-string read-line) :write? :memory}
           4 {:arity :unary :fn println :write? nil}
           5 {:arity :unary :fn #(not= % 0) :write? :ip}
@@ -33,16 +32,19 @@
 
 (defn num->op-code [num]
   (let [temp (str num)
-        op (str
-            (s/join (repeat (- 5 (count temp)) "0"))
-            temp)
+        op (as-> temp $
+             (count $)
+             (repeat (- 5 $) "0")
+             (s/join $)
+             (str $ temp))
         code-str (subs op 3)
         code (if (not= code-str "99")
                (-> code-str last str read-string)
                99)
-        modes (->> (-> op (subs 0 3) (s/split #""))
-                   (map read-string)
-                   reverse)]
+        modes (as-> op $
+                (subs $ 0 3)
+                (s/split $ #"")
+                (map read-string $) (reverse $))]
     {:code code :param-modes modes}))
 
 (defn num-params [arity]
@@ -51,31 +53,36 @@
 (defn next-ip [{:keys [arity write?] :as op} ip]
   (when op
     (let [param-count (num-params arity)
-          write-count (if (= write? :memory) 1 0)]
+          write-count (if write? 1 0)]
       (+ ip 1 param-count write-count))))
 
 (defn next-state
   "Advance program state to its next state"
   [{:keys [ip memory modes ops] :as state}]
-  (println (str memory " @ " ip))
+  ;;(println (str memory " @ " ip))
   (let [{:keys [code param-modes]} (num->op-code (memory ip))
+        ;; Helper that uses given mode to look something up in memory
+        ;; at the given offset from the instruction pointer
         param-lookup (fn [[off mode]]
                        (let [mode-fn (get modes mode)]
                          (mode-fn memory (+ ip off))))]
-    (when (not= code 99)
+    (when (and (not= code 99) (get ops code))
       (let [{:keys [arity fn write?] :as op} (get ops code)
             np (num-params arity)
             offsets (take np (drop 1 (range)))
             offsets-with-mode (zipmap offsets param-modes)
             params (map param-lookup offsets-with-mode)
             result (apply fn params)
-            result-loc (memory (+ ip np 1))]
+            write-offset (+ np 1)]
         (merge state
                {:memory (if (= write? :memory)
-                          (assoc memory result-loc result)
+                          (assoc memory
+                                 (param-lookup [write-offset 1])
+                                 result)
                           memory)
                 :ip (if (and (= write? :ip) result)
-                      result-loc
+                      (param-lookup [write-offset
+                                     (first (drop np param-modes))])
                       (next-ip op ip))})))))
 
 (defn run-program
@@ -85,8 +92,7 @@
     (run-program state (next-state state))
     prev-state))
 
-;; TODO: get op-codes 5 & 6 working
 ;; NOTE: when prompted for input provide 1 for part 1 and 5 for part 2
-(->> [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
+(->> data
      initial-state
      (run-program nil))
