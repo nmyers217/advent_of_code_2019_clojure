5314654695dcc6baf8ba4844b20bc7e240dd6da2
nmyers217 <nickbmyers217@gmail.com>
2019-12-12 13:10:33 -0600

    Refactor: clean up day 12 part 1 a bit

--- src/aoc_2019/day_12.clj
+++ src/aoc_2019/day_12.clj
@@ -3,10 +3,10 @@
 
 (def input (-> "day_12.txt" io/resource io/reader line-seq))
 
-(def test-input ["<x=-8, y=-10, z=0>"
-                 "<x=5, y=5, z=10>"
-                 "<x=2, y=-7, z=3>"
-                 "<x=9, y=-8, z=-3>"])
+(def test-input ["<x=-1, y=0, z=2>"
+                 "<x=2, y=-10, z=-7>"
+                 "<x=4, y=-8, z=8>"
+                 "<x=3, y=5, z=-1>"])
 
 (defn read-vec [vec-str]
   (->> vec-str
@@ -26,33 +26,30 @@
         (< n1 n2) 1
         (> n1 n2) -1))
 
-(defn next-velocity-component [n v ns]
+(defn transpose [m]
+  (apply mapv vector m))
+
+(defn next-vel-value [ns n v]
   (reduce #(+ %1 (gravity-delta n %2)) v ns))
+
+(defn next-vel-component [pos-component vel-component]
+  (map-indexed (fn [i v] (next-vel-value pos-component (pos-component i) v))
+               vel-component))
+
+(defn next-moons [moons]
+  (let [pos-components (transpose (map :pos moons))
+        vel-components (transpose (map :vel moons))
+        new-vel-comps  (map next-vel-component pos-components vel-components)
+        new-vels       (transpose new-vel-comps)
+        new-ps         (mapv #(mapv + %1 %2) (map :pos moons) new-vels)
+        next-moon      (fn [moon pos vel] (merge moon {:pos pos :vel vel}))]
+    (mapv (partial apply next-moon)
+          (partition 3 (interleave moons new-ps new-vels)))))
 
 (defn abs-sum [v]
   (->> v
        (map #(Math/abs %))
        (reduce +)))
-
-;; FIXME i feel like there was a way more elegant way to do this, but i couldn't figure it out
-(defn next-moons [moons]
-  (let [get-component  (fn [c] (mapv #((:pos %) c) moons))
-        components     (mapv get-component (range 0 3))
-        gravity-deltas (map-indexed
-                        (fn [vel-c comps]
-                          (map-indexed
-                           (fn [moon-i c]
-                             (next-velocity-component c (get-in moons [moon-i :vel vel-c]) comps))
-                           comps))
-                        components)
-        positions      (mapv :pos moons)
-        new-velocities (mapv (fn [moon-i] (mapv #(nth % moon-i) gravity-deltas))
-                             (range (count moons)))
-        new-positions  (mapv #(mapv + %1 %2) positions new-velocities)
-        ]
-    (mapv merge
-          (mapv #(hash-map :pos %) new-positions)
-          (mapv #(hash-map :vel %) new-velocities))))
 
 (defn total-energy [moons]
   (let [pot (map (comp abs-sum :pos) moons)
@@ -68,5 +65,9 @@
        last
        total-energy
        println))
+
+;; TODO find out how long it takes for one moon to do its orbit and reach its original position ;; TODO if that works then do it for the other moons
+;; TODO find the least common multiple of their respective cycles
+;; TODO that number should be the total amount of the time for the cycle of the overall system
 
 (do (part-one))
